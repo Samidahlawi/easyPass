4b6dcab684a9377b99ae797bcbd37be8af23877e
samidahlawi <o.t5@hotmail.com>
2020-04-23 06:20:36 +0300

    finish project v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Timer from './Components/Timer/Timer.js';
+import Header from './Components/Header/Header'
+import Qrcode from './Components/Qrcode/Qrcode'
+import Profile from './Components/Profile/Profile'
+import {Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
+      <Route exact path='/'>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header  />
+        
       </header>
+
+      <main className="main">
+        <Timer />
+      </main>
+
+          
+      <main >
+       
+       </main>
+       <main >
+       
+       </main>
+
+      <footer className="footer">
+          <Qrcode />
+      </footer>
+      </Route>
+      <Route path='/profile'>
+        <Profile/>
+      </Route>
     </div>
   );
 }
---
+++ src/Components/Qrcode/Qrcode.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import QRcode from './QR-code.png'
+import {Link} from 'react-router-dom'
+
+class Qrcode extends Component{
+    render(){
+        return(
+            <div>
+                <Link to="/profile">
+                   <img className="qr-code" src={QRcode} alt="QRcode" />
+                   </Link>
+            </div>
+        )
+    }
+
+}
+
+export default Qrcode;
